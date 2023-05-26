ac823a1e6367bb5449dda5c038f9787f96a5068e
LedSquare <prophet3334@gmail.com>
2023-05-26 21:37:01 +0300

    change helper

--- app/helper.php
+++ app/helper.php
@@ -1,12 +1,5 @@
 <?php
 
-if (!function_exists('activelink')) {
-    function activeLink($urla)
-    {
-        $url = url()->current();
-        $class = 'active_a';
-        return $url == "http://blog.ru/ " . "$urla" ? $class : '';
-    }
-}
+
 
 
